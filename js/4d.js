8a72179011cd278529b0cdd7fe9c6ea01f6212fd
unknown <kidosol2169@gmail.com>
2020-06-11 15:12:12 +0800

    update on website

---
+++ js/4d.js
@@ -0,0 +1,175 @@
+
+let screenDistancew = 1; //determined from player's direction, usually right in front of player
+
+let zoom = 5;
+let resizerw = 100; //resize the projected points
+
+let playerPos = [0, 0, 0, -200];
+
+let angles = [0, 0, 0, 0, 0, 0] ;
+let angleSpeeds = [0, 0.05, 0, 0, 0.05, 0];  
+
+
+function rotation4xy(x, y, z, w, a){
+    return [x * Math.cos(a) + y * (-Math.sin(a)),
+        x * Math.sin(a) + y * Math.cos(a),
+        z,
+        w];
+}
+function rotation4xz(x, y, z, w, a){
+    return [x * Math.cos(a) + z * (-Math.sin(a)),
+            y,
+            x * Math.sin(a) + z * Math.cos(a),
+            w];
+}
+function rotation4xw(x, y, z, w, a){
+    return [x * Math.cos(a) + w * (-Math.sin(a)),
+            y,
+            z,
+            x * Math.sin(a) + w * Math.cos(a)];
+}
+function rotation4yz(x, y, z, w, a){
+    return [x,
+            y * Math.cos(a) + z * (-Math.sin(a)),
+            y * Math.sin(a) + z * Math.cos(a),
+            w];
+}
+function rotation4yw(x, y, z, w, a){
+    return [x,
+            y * Math.cos(a) + w * (-Math.sin(a)),
+            z,
+            y * Math.sin(a) + w * Math.cos(a)];
+}
+function rotation4zw(x, y, z, w, a){
+    return [x,
+        y,
+        z * Math.cos(a) + (w * -Math.sin(a)),
+        z * Math.sin(a) + (w * Math.cos(a))];
+}
+
+
+// Point = function(x=null, y=null, z=null, w=null){
+Point = function(coordinate){
+    this.origx = coordinate[0];
+    this.origy = coordinate[1];
+    this.origz = coordinate[2];
+    this.origw = coordinate[3];
+    this.x = coordinate[0];
+    this.y = coordinate[1];
+    this.z = coordinate[2];
+    this.w = coordinate[3];
+    // this.dimension = (x!=null) + (y!=null) + (z!=null) + (w!=null);
+
+    this.draw = function(){
+        projectedPoint = project4d(this.x, this.y, this.z, this.w)
+        // d = ((projectedPoint[0]-playerPos[0])**2+(projectedPoint[1]-playerPos[1])**2+(projectedPoint[2]-playerPos[2])**2)**0.5
+        // stroke(d*5)
+        vertex(projectedPoint[0] * zoom, projectedPoint[1] * zoom, projectedPoint[2] * zoom)
+    }
+
+    this.rotate = function(axy, axz, axw, ayz, ayw, azw){
+        rotated1 = rotation4xy(this.origx, this.origy, this.origz, this.origw, axy)
+        rotated2 = rotation4xz(rotated1[0], rotated1[1], rotated1[2], rotated1[3], axz)
+        rotated3 = rotation4xw(rotated2[0], rotated2[1], rotated2[2], rotated2[3], axw)
+        rotated4 = rotation4yz(rotated3[0], rotated3[1], rotated3[2], rotated3[3], ayz)
+        rotated5 = rotation4yw(rotated4[0], rotated4[1], rotated4[2], rotated4[3], ayw)
+        rotated6 = rotation4zw(rotated5[0], rotated5[1], rotated5[2], rotated5[3], azw)
+        this.x = rotated6[0];
+        this.y = rotated6[1];
+        this.z = rotated6[2];
+        this.w = rotated6[3];
+    }
+}
+
+let data = []
+
+function cube(vertices){
+    data.push([new Point(vertices[0]), new Point(vertices[1]), new Point(vertices[2]), new Point(vertices[3])])
+    data.push([new Point(vertices[2]), new Point(vertices[3]), new Point(vertices[4]), new Point(vertices[5])])
+    data.push([new Point(vertices[4]), new Point(vertices[5]), new Point(vertices[6]), new Point(vertices[7])])
+    data.push([new Point(vertices[0]), new Point(vertices[1]), new Point(vertices[6]), new Point(vertices[7])])
+    data.push([new Point(vertices[1]), new Point(vertices[2]), new Point(vertices[5]), new Point(vertices[6])])
+    data.push([new Point(vertices[0]), new Point(vertices[3]), new Point(vertices[4]), new Point(vertices[7])])
+}3
+
+function tesseract(x, y, z, w, len){
+    cube([[x,y,z,w], [x+len,y,z,w], [x+len,y+len,z,w], [x,y+len,z,w], [x,y+len,z+len,w], [x+len,y+len,z+len,w], [x+len,y,z+len,w], [x,y,z+len,w]])
+    cube([[x,y,z,w], [x,y+len,z,w], [x,y+len,z+len,w], [x,y,z+len,w], [x,y,z+len,w+len], [x,y+len,z+len,w+len], [x,y+len,z,w+len], [x,y,z,w+len]])
+    cube([[x,y,z,w], [x,y,z+len,w], [x,y,z+len,w+len], [x,y,z,w+len], [x+len,y,z,w+len], [x+len,y,z+len,w+len], [x+len,y,z+len,w], [x+len,y,z,w]])
+    cube([[x,y,z,w], [x,y,z,w+len], [x+len,y,z,w+len], [x+len,y,z,w], [x+len,y+len,z,w], [x+len,y+len,z,w+len], [x,y+len,z,w+len], [x,y+len,z,w]])
+    x+=len;y+=len;z+=len;w+=len;
+    len = -len
+    cube([[x,y,z,w], [x+len,y,z,w], [x+len,y+len,z,w], [x,y+len,z,w], [x,y+len,z+len,w], [x+len,y+len,z+len,w], [x+len,y,z+len,w], [x,y,z+len,w]])
+    cube([[x,y,z,w], [x,y+len,z,w], [x,y+len,z+len,w], [x,y,z+len,w], [x,y,z+len,w+len], [x,y+len,z+len,w+len], [x,y+len,z,w+len], [x,y,z,w+len]])
+    cube([[x,y,z,w], [x,y,z+len,w], [x,y,z+len,w+len], [x,y,z,w+len], [x+len,y,z,w+len], [x+len,y,z+len,w+len], [x+len,y,z+len,w], [x+len,y,z,w]])
+    cube([[x,y,z,w], [x,y,z,w+len], [x+len,y,z,w+len], [x+len,y,z,w], [x+len,y+len,z,w], [x+len,y+len,z,w+len], [x,y+len,z,w+len], [x,y+len,z,w]])
+
+}
+
+function cantor(x, y, z, w, len, depth){
+    if (depth <= 0){
+        tesseract(x,y,z,w,len)
+    }else{
+        for (var i = 0; i<2; i++){
+            for (var j = 0; j<2; j++){
+                for (var k = 0; k<2; k++){
+                    for (var l = 0; l<2; l++){
+                        cantor(x + (2*(len/3)*i), y + (2*(len/3)*j), z + (2*(len/3)*k), w + (2*(len/3)*l), len/3, depth-1)
+                    }
+                }
+            }
+        }
+    }
+}
+
+cantor(-50, -50, -50, -50, 100, 1)
+// tesseract(-50, -50, -50, -50, 100)
+
+
+//=====projectors=====
+
+function project4d(x, y, z, w){
+    return [((x-playerPos[0])/(w-playerPos[3]))*screenDistancew * resizerw, 
+            ((y-playerPos[1])/(w-playerPos[3]))*screenDistancew * resizerw, 
+            ((z-playerPos[2])/(w-playerPos[3]))*screenDistancew * resizerw,
+            null ]
+}
+
+//==========
+
+function setup(){
+    createCanvas(500,500, WEBGL);
+    strokeWeight(1);
+    stroke(255);
+    // // noFill();
+    fill(255, 0, 255, 10);
+    // fill(255, 0, 0);
+}
+
+function draw(){
+    background(0);
+    for (let i = 0; i < angles.length; i++){
+        angles[i] += angleSpeeds[i];
+    }
+    for (const hyperobject of data){
+        beginShape();
+        for (const point of hyperobject){
+            point.rotate(angles[0], angles[1], angles[2], angles[3], angles[4], angles[5])
+            point.draw()
+        }
+        endShape(CLOSE);
+    }
+    // debugger
+    
+    if(keyIsDown(16)) playerPos[1] += 5;
+    if(keyIsDown(32)) playerPos[1] -= 5;
+    
+    if(keyIsDown(87)) playerPos[2] -= 5;
+    if(keyIsDown(83)) playerPos[2] += 5;
+    
+    if(keyIsDown(68)) playerPos[0] += 5;
+    if(keyIsDown(65)) playerPos[0] -= 5;
+
+    if(keyIsDown(81)) playerPos[3] += 5;
+    if(keyIsDown(69)) playerPos[3] -= 5;
+}
---
+++ js/spiral.js
@@ -0,0 +1,115 @@
+
+let vertices = [];
+
+let size = 2;
+let nextSize = 2;
+let radiusFactor = 3;
+let maxDepth = 3;
+let motherSize = 100;
+let nextMotherSize = 100;
+
+let circleDistance = 0;
+let nextCircleDistance = 0;
+
+let showCircle = true;
+let showPath = true;
+
+let circleId = -1;
+
+function updateSpirograph(){
+  circleId = -1
+    vertices = [];
+    size = nextSize;
+    circleDistance = nextCircleDistance;
+    motherSize = nextMotherSize;
+    mom = new Circle(200,200, motherSize, maxDepth);
+    mom.createChild();
+}
+
+Circle = function(x, y, r, depth){
+  this.x = x;
+  this.y = y;
+  this.radius = r;
+  this.angle = 0;
+  this.pa = 0;
+  this.depth = depth;
+
+  this.child = []
+  
+  this.id;
+  if (this.depth == 0){
+    circleId ++;
+    this.id = circleId
+    vertices.push([])
+  }
+
+  this.draw = function(){
+    ellipse(this.x, this.y, this.radius*2);
+    for (var i in this.child){
+      this.child[i].draw();
+    }
+  }
+
+  this.createChild = function(){
+    if (this.depth > 0){
+      for (var i = 0; i<size; i++){
+        angle = Math.PI*2/size
+        this.child.push(new Circle(this.x+Math.cos(angle*i)*this.radius, this.y+Math.sin(angle*i)*radius, this.radius/radiusFactor, this.depth-1))
+        this.child[this.child.length-1].createChild();
+      }
+    }
+  }
+
+  this.rotateChild = function(){
+    for (var i in this.child){
+      angle = Math.PI*2/size
+      this.child[i].x = (this.x + (Math.cos(angle*i + this.angle + this.pa)*(this.radius+this.child[i].radius+(circleDistance * (this.radius/motherSize)))))
+      this.child[i].y = (this.y + (Math.sin(angle*i + this.angle + this.pa)*(this.radius+this.child[i].radius+(circleDistance * (this.radius/motherSize)))))
+      this.child[i].angle += 0.01
+      this.child[i].pa = this.angle + angle*i + this.pa
+      this.child[i].rotateChild()
+      if (this.depth == 1){
+        if (mom.angle < (Math.PI*2)/size){  
+            vertices[this.child[i].id].push([this.child[i].x, this.child[i].y])
+        }
+      }
+    }
+    this.angle += 0.01;
+  }
+}
+
+let mom;
+function setup() {
+  canvas = createCanvas(400, 400);
+  canvas.parent("sketch-holder")
+  mom = new Circle(200,200, motherSize, 2);
+  mom.createChild();
+}
+
+function draw() {
+  background(220);
+  strokeWeight(1);
+
+  if (showPath){
+    for (var i in vertices){
+    // for (var i in [0]){
+      beginShape();
+      for (var j in vertices[i]){
+        vert = vertices[i][j];
+        vertex(vert[0], vert[1] , 0)
+      }
+      endShape();
+      // // console.log(i)
+      //  vert = vertices[i];
+      //  vertex(vert[0], vert[1] , 0)
+    }
+    // updatePixels();
+  }
+  
+  noFill();
+  strokeWeight(5);
+  if (showCircle){
+    mom.draw();
+  }
+  mom.rotateChild();
+}
